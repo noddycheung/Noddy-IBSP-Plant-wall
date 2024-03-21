7509118849d4ee11e2d23becfcae8407c247fe5e
Cheung Tsz Chun Noddy <118799146+noddycheung@users.noreply.github.com>
2024-03-21 16:58:35 +0800

    Add files via upload

--- Plant Wall Design Project 3 soil sensor/src/main.cpp
+++ Plant Wall Design Project 3 soil sensor/src/main.cpp
@@ -73,87 +73,7 @@ void controlSensor(int humidity, int targetHumidity, FlowSensor &flowSensor, Sol
   }
 }
 
-// void readAndSendSensorData() {
-//     int temperature = mySoilSensor.TemperatureValue();
-//     int humidity = mySoilSensor.HumidityValue();
-//     int conductivity = mySoilSensor.conductivity();
-//     int pHValue = mySoilSensor.pH();
-//     int nitrogenValue = mySoilSensor.nitrogen();
-//     int phosphorusValue = mySoilSensor.Phosphorus();
-//     int potassiumValue = mySoilSensor.Potassium();
-    
-//     int temperature2 = mySoilSensor.TemperatureValue2();
-//     int humidity2 = mySoilSensor.HumidityValue2();
-//     int conductivity2 = mySoilSensor.conductivity2();
-//     int pHValue2 = mySoilSensor.pH2();
-//     int nitrogenValue2 = mySoilSensor.nitrogen2();
-//     int phosphorusValue2 = mySoilSensor.Phosphorus2();
-//     int potassiumValue2 = mySoilSensor.Potassium2();
-
-//     int temperature3 = mySoilSensor.TemperatureValue3();
-//     int humidity3 = mySoilSensor.HumidityValue3();
-//     int conductivity3 = mySoilSensor.conductivity3();
-//     int pHValue3 = mySoilSensor.pH3();
-//     int nitrogenValue3 = mySoilSensor.nitrogen3();
-//     int phosphorusValue3 = mySoilSensor.Phosphorus3();
-//     int potassiumValue3 = mySoilSensor.Potassium3();
-
-//     int waterLevel = waterLevelSensor.getDistance();
-//     byte SuperPlant[45];
-//     SuperPlant[ 0] = (byte)0xff;
-//     SuperPlant[ 1] = (byte)((temperature & 0xFF00) >> 8);
-//     SuperPlant[ 2] = (byte)((temperature & 0x00FF));
-//     SuperPlant[ 3] = (byte)((humidity & 0xFF00) >> 8);
-//     SuperPlant[ 4] = (byte)((humidity & 0x00FF));
-//     SuperPlant[ 5] = (byte)((conductivity & 0xFF00) >> 8);
-//     SuperPlant[ 6] = (byte)((conductivity & 0x00FF));
-//     SuperPlant[ 7] = (byte)((pHValue & 0xFF00) >> 8);
-//     SuperPlant[ 8] = (byte)((pHValue & 0x00FF));
-//     SuperPlant[ 9] = (byte)((nitrogenValue & 0xFF00) >> 8);
-//     SuperPlant[10] = (byte)((nitrogenValue & 0x00FF));
-//     SuperPlant[11] = (byte)((phosphorusValue & 0xFF00) >> 8);
-//     SuperPlant[12] = (byte)((phosphorusValue & 0x00FF));
-//     SuperPlant[13] = (byte)((potassiumValue & 0xFF00) >> 8);
-//     SuperPlant[14] = (byte)((potassiumValue & 0x00FF));
-//     SuperPlant[15] = (byte)((waterLevel & 0xFF00) >> 8);
-//     SuperPlant[16] = (byte)((waterLevel & 0x00FF));
-//     SuperPlant[17] = (byte)((temperature2 & 0xFF00) >> 8);
-//     SuperPlant[18] = (byte)((temperature2 & 0x00FF));
-//     SuperPlant[19] = (byte)((humidity2 & 0xFF00) >> 8);
-//     SuperPlant[20] = (byte)((humidity2 & 0x00FF));
-//     SuperPlant[21] = (byte)((conductivity2 & 0xFF00) >> 8);
-//     SuperPlant[22] = (byte)((conductivity2 & 0x00FF));
-//     SuperPlant[23] = (byte)((pHValue2 & 0xFF00) >> 8);
-//     SuperPlant[24] = (byte)((pHValue2 & 0x00FF));
-//     SuperPlant[25] = (byte)((nitrogenValue2 & 0xFF00) >> 8);
-//     SuperPlant[26] = (byte)((nitrogenValue2 & 0x00FF));
-//     SuperPlant[27] = (byte)((phosphorusValue2 & 0xFF00) >> 8);
-//     SuperPlant[28] = (byte)((phosphorusValue2 & 0x00FF));
-//     SuperPlant[29] = (byte)((potassiumValue2 & 0xFF00) >> 8);
-//     SuperPlant[30] = (byte)((potassiumValue2 & 0x00FF));
-//     SuperPlant[31] = (byte)((temperature3 & 0xFF00) >> 8);
-//     SuperPlant[32] = (byte)((temperature3 & 0x00FF));
-//     SuperPlant[33] = (byte)((humidity3 & 0xFF00) >> 8);
-//     SuperPlant[34] = (byte)((humidity3 & 0x00FF));
-//     SuperPlant[35] = (byte)((conductivity3 & 0xFF00) >> 8);
-//     SuperPlant[36] = (byte)((conductivity3 & 0x00FF));
-//     SuperPlant[37] = (byte)((pHValue3 & 0xFF00) >> 8);
-//     SuperPlant[38] = (byte)((pHValue3 & 0x00FF));
-//     SuperPlant[39] = (byte)((nitrogenValue3 & 0xFF00) >> 8);
-//     SuperPlant[40] = (byte)((nitrogenValue3 & 0x00FF));
-//     SuperPlant[41] = (byte)((phosphorusValue3 & 0xFF00) >> 8);
-//     SuperPlant[42] = (byte)((phosphorusValue3 & 0x00FF));
-//     SuperPlant[43] = (byte)((potassiumValue3 & 0xFF00) >> 8);
-//     SuperPlant[44] = (byte)((potassiumValue3 & 0x00FF));
-//     Serial.write(SuperPlant, sizeof(SuperPlant));
-//     controlSensor(humidity, humidity_target, flowSensor, Valve1);
-//     controlSensor(humidity2, humidity_target, flowSensor2, Valve2);
-//     controlSensor(humidity3, humidity_target, flowSensor3, Valve3);
-// }
-
-
 void loop() {  
-  // readAndSendSensorData();
     int temperature = mySoilSensor.TemperatureValue();
     int humidity = mySoilSensor.HumidityValue();
     int conductivity = mySoilSensor.conductivity();
@@ -235,7 +155,6 @@ void loop() {
     ClosePump();
   }else {
     OpenPump();
-    // readAndSendSensorData();
     int temperature = mySoilSensor.TemperatureValue();
     int humidity = mySoilSensor.HumidityValue();
     int conductivity = mySoilSensor.conductivity();
